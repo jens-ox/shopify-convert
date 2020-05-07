6443a2e34870ddb52daf0414121f644924d434f9
Jens Ox <github@jens-ox.de>
2020-05-07 10:06:18 +0200

    correctly handle 0-value in compare at price

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,11 +5,8 @@ import { convertArrayToCSV } from 'convert-array-to-csv'
 const rawHeader = ['Handle', 'Title', 'Body (HTML)', 'Vendor', 'Type', 'Tags', 'Published', 'Option1 Name', 'Option1 Value', 'Option2 Name', 'Option2 Value', 'Option3 Name', 'Option3 Value', 'Variant SKU', 'Variant Grams', 'Variant Inventory Tracker', 'Variant Inventory Qty', 'Variant Inventory Policy', 'Variant Fulfillment Service', 'Variant Price', 'Variant Compare At Price', 'Variant Requires Shipping', 'Variant Taxable', 'Variant Barcode', 'Image Src', 'Image Position', 'Image Alt Text', 'Gift Card', 'SEO Title', 'SEO Description', 'Google Shopping / Google Product Category', 'Google Shopping / Gender', 'Google Shopping / Age Group', 'Google Shopping / MPN', 'Google Shopping / AdWords Grouping', 'Google Shopping / AdWords Labels', 'Google Shopping / Condition', 'Google Shopping / Custom Product', 'Google Shopping / Custom Label 0', 'Google Shopping / Custom Label 1', 'Google Shopping / Custom Label 2', 'Google Shopping / Custom Label 3', 'Google Shopping / Custom Label 4', 'Variant Image', 'Variant Weight Unit', 'Variant Tax Code', 'Cost per item']
 
 const customParsers = [{
-  name: 'Body (HTML)',
-  parse: entry => {
-    console.log('parsing: ', entry)
-    return entry
-  }
+  name: 'Variant Compare At Price',
+  parse: entry => entry !== 0 ? entry : ''
 }]
 
 const defaults = [{
@@ -35,10 +32,7 @@ const defaults = [{
   value: () => false
 }, {
   name: 'Handle',
-  value: (entry) => {
-    console.log('generating handle for: ', entry)
-    return null
-  }
+  value: () => null
 }, {
   name: 'Option1 Name',
   value: () => 'Title'
